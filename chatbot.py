d2a069040703aa53088d3209b83f4c5dfc68a1bc
Poojamerkala <poojamerkala2006@gmail.com>
2026-03-03 14:45:56 +0530

    Initial commit - Rule Based Chatbot AI

---
+++ chatbot.py
@@ -0,0 +1,22 @@
+import re
+
+print("AI Chatbot started. Type 'exit' to quit.")
+
+while True:
+    user_input = input("You: ").lower()
+
+    if re.search(r"\b(hi|hello|hey)\b", user_input):
+        print("Chatbot: Hello! 😊")
+    
+    elif re.search(r"\bhow are you\b", user_input):
+        print("Chatbot: I am GOOD!")
+    
+    elif re.search(r"\byour name\b", user_input):
+        print("Chatbot: I am an AI Rule-Based Chatbot.")
+    
+    elif user_input in ["exit", "bye"]:
+        print("Chatbot: Goodbye! THANKS TO MEET YOU 👋")
+        break
+    
+    else:
+        print("Chatbot: I don't understand that.")
